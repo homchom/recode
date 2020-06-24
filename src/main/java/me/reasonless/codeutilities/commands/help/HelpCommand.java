863e3e17ce0cc87aad4457f2afb6b7c4cfcea774
Reasonless <reasonlessmc@gmail.com>
2020-06-24 11:13:31 +0100

    Update HelpCommand.java

--- src/main/java/me/reasonless/codeutilities/commands/help/HelpCommand.java
+++ src/main/java/me/reasonless/codeutilities/commands/help/HelpCommand.java
@@ -12,15 +12,15 @@ public class HelpCommand {
 	static String version = Main.MOD_VERSION;
 	
 	public static int execute(MinecraftClient mc, CommandContext<CottonClientCommandSource> ctx) {
-		mc.player.sendMessage(new LiteralText(MinecraftColors.GRAY + "" + MinecraftColors.STRIKETHROUGH + "========================================="));
-		mc.player.sendMessage(new LiteralText(MinecraftColors.DARK_AQUA + "" + MinecraftColors.BOLD + "Code" + MinecraftColors.AQUA + "" + MinecraftColors.BOLD + "Utilities" + MinecraftColors.GRAY + " - " + MinecraftColors.RESET + "v" + version));
-		mc.player.sendMessage(new LiteralText(" "));
-		mc.player.sendMessage(new LiteralText(MinecraftColors.YELLOW + "/codeutilities music"));
-        mc.player.sendMessage(new LiteralText("Shows all music commands"));
-        mc.player.sendMessage(new LiteralText(" "));
-        mc.player.sendMessage(new LiteralText(MinecraftColors.YELLOW + "/codeutilities image"));
-        mc.player.sendMessage(new LiteralText("Shows all image commands"));
-		mc.player.sendMessage(new LiteralText(MinecraftColors.GRAY + "" + MinecraftColors.STRIKETHROUGH + "========================================="));
+		mc.player.sendMessage(new LiteralText(MinecraftColors.GRAY + "" + MinecraftColors.STRIKETHROUGH + "========================================="), false);
+		mc.player.sendMessage(new LiteralText(MinecraftColors.DARK_AQUA + "" + MinecraftColors.BOLD + "Code" + MinecraftColors.AQUA + "" + MinecraftColors.BOLD + "Utilities" + MinecraftColors.GRAY + " - " + MinecraftColors.RESET + "v" + version), false);
+		mc.player.sendMessage(new LiteralText(" "), false);
+		mc.player.sendMessage(new LiteralText(MinecraftColors.YELLOW + "/codeutilities music"), false);
+		mc.player.sendMessage(new LiteralText("Shows all music commands"), false);
+		mc.player.sendMessage(new LiteralText(" "), false);
+		mc.player.sendMessage(new LiteralText(MinecraftColors.YELLOW + "/codeutilities image"), false);
+		mc.player.sendMessage(new LiteralText("Shows all image commands"), false);
+		mc.player.sendMessage(new LiteralText(MinecraftColors.GRAY + "" + MinecraftColors.STRIKETHROUGH + "========================================="), false);
 		return 1;
 	}
 }
