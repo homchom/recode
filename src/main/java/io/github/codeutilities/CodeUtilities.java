04c4f16f2a1efb6986537ae28194b9502543ff2a
BlazeMCworld <62186782+BlazeMCworld@users.noreply.github.com>
2020-07-10 17:29:31 +0200

    Give Command
    added the /give command

--- src/main/java/io/github/codeutilities/CodeUtilities.java
+++ src/main/java/io/github/codeutilities/CodeUtilities.java
@@ -1,29 +1,40 @@
 package io.github.codeutilities;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.LiteralText;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class CodeUtilities implements ModInitializer {
 
-    public static Logger LOGGER = LogManager.getLogger();
+  public static final String MOD_ID = "codeutilities";
+  public static final String MOD_NAME = "CodeUtilities";
+  public static final String SONG_PARSER_VERSION = "4"; //NBS parser version
+  public static final String SONG_NBS_FORMAT_VERSION = "4"; //NBS format version
+  public static Logger LOGGER = LogManager.getLogger();
+  public static MinecraftClient mc = MinecraftClient.getInstance();
 
-    public static final String MOD_ID = "codeutilities";
-    public static final String MOD_NAME = "CodeUtilities";
+  public static void log(Level level, String message) {
+    LOGGER.log(level, "[" + MOD_NAME + "] " + message);
+  }
 
-    public static final String SONG_PARSER_VERSION = "4"; //NBS parser version
-	public static final String SONG_NBS_FORMAT_VERSION = "4"; //NBS format version
-    
-    @Override
-    public void onInitialize() {
-        log(Level.INFO, "Initializing");
-        //TODO: Initializer
-    }
+  public static void chat(String text) {
+    mc.player.sendMessage(new LiteralText(text), false);
+  }
 
-    public static void log(Level level, String message){
-        LOGGER.log(level, "["+MOD_NAME+"] " + message);
-    }
+  @Override
+  public void onInitialize() {
+    log(Level.INFO, "Initializing");
+    //TODO: Initializer
+  }
+
+  public static void giveCreativeItem(ItemStack item) {
+    assert mc.interactionManager != null;
+    assert mc.player != null;
+    mc.interactionManager.clickCreativeStack(item, 36 + mc.player.inventory.getSwappableHotbarSlot());
+  }
 
 }
