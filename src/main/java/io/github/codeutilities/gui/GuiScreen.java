9b9c5afffc4b54bc0f96505077381de5f9facc1c
BlazeMCworld <62186782+BlazeMCworld@users.noreply.github.com>
2020-07-11 12:44:50 +0200

    Added the a subclass of ClientCottonScreen cuz they said so in the video

---
+++ src/main/java/io/github/codeutilities/gui/GuiScreen.java
@@ -0,0 +1,10 @@
+package io.github.codeutilities.gui;
+
+import io.github.cottonmc.cotton.gui.GuiDescription;
+import io.github.cottonmc.cotton.gui.client.CottonClientScreen;
+
+public class GuiScreen extends CottonClientScreen {
+   public GuiScreen(GuiDescription description) {
+      super(description);
+   }
+}
