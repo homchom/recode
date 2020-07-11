e83b80bce6109eafac85282a33c2d324c25aed50
BlazeMCworld <62186782+BlazeMCworld@users.noreply.github.com>
2020-07-11 12:33:06 +0200

    (Failed) Attempt at adding a gui

--- src/main/java/io/github/codeutilities/commands/Commands.java
+++ src/main/java/io/github/codeutilities/commands/Commands.java
@@ -1,12 +1,15 @@
 package io.github.codeutilities.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
-
 import io.github.codeutilities.commands.item.GiveCommand;
 import io.github.codeutilities.commands.item.LoreCommand;
 import io.github.codeutilities.commands.nbs.NBSCommand;
+import io.github.codeutilities.gui.ExampleGui;
+import io.github.cottonmc.clientcommands.ArgumentBuilders;
 import io.github.cottonmc.clientcommands.ClientCommandPlugin;
 import io.github.cottonmc.clientcommands.CottonClientCommandSource;
+import io.github.cottonmc.cotton.gui.client.CottonClientScreen;
+import net.minecraft.client.MinecraftClient;
 
 public class Commands implements ClientCommandPlugin {
 
@@ -15,5 +18,10 @@ public class Commands implements ClientCommandPlugin {
       GiveCommand.register(cd);
       LoreCommand.register(cd);
       NBSCommand.register(cd);
+
+      cd.register(ArgumentBuilders.literal("guitest").executes(ctx -> {
+         MinecraftClient.getInstance().openScreen(new CottonClientScreen(new ExampleGui()));
+         return 1;
+      }));
    }
 }
