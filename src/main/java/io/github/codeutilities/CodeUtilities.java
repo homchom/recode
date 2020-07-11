259deb9272830c87f82cd50b284cb0a46c5e9dba
BlazeMCworld <62186782+BlazeMCworld@users.noreply.github.com>
2020-07-11 13:10:14 +0200

    Added an ExampleGui

--- src/main/java/io/github/codeutilities/CodeUtilities.java
+++ src/main/java/io/github/codeutilities/CodeUtilities.java
@@ -1,48 +1,59 @@
 package io.github.codeutilities;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import io.github.codeutilities.config.ModConfig;
-import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
-import me.sargunvohra.mcmods.autoconfig1u.ConfigHolder;
-import me.sargunvohra.mcmods.autoconfig1u.serializer.DummyConfigSerializer;
+import io.github.cottonmc.cotton.gui.client.CottonClientScreen;
+import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.LiteralText;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class CodeUtilities implements ModInitializer {
 
-  public static final String MOD_ID = "codeutilities";
-  public static final String MOD_NAME = "CodeUtilities";
-  public static final String SONG_PARSER_VERSION = "4"; //NBS parser version
-  public static final String SONG_NBS_FORMAT_VERSION = "4"; //NBS format version
-  public static Logger LOGGER = LogManager.getLogger();
-  public static MinecraftClient mc = MinecraftClient.getInstance();
+   public static final String MOD_ID = "codeutilities";
+   public static final String MOD_NAME = "CodeUtilities";
+   public static final String SONG_PARSER_VERSION = "4"; //NBS parser version
+   public static final String SONG_NBS_FORMAT_VERSION = "4"; //NBS format version
+   public static Logger LOGGER = LogManager.getLogger();
+   public static MinecraftClient mc = MinecraftClient.getInstance();
 
-  @Override
-  public void onInitialize() {
-    log(Level.INFO, "Initializing");
+   public static void openGuiAsync(LightweightGuiDescription gui) {
+      new Thread(() -> {
+         try {
+            Thread.sleep(0);
+         } catch (InterruptedException e) {
+            e.printStackTrace();
+         }
+         openGuiSync(gui);
+      }).start();
+   }
 
-    ConfigHolder<ModConfig> holder = AutoConfig.register(
-    	ModConfig.class,
-    	DummyConfigSerializer::new
-    );
-  }
+   public static void openGuiSync(LightweightGuiDescription gui) {
+      MinecraftClient.getInstance().openScreen(new CottonClientScreen(gui));
+   }
 
-  public static void giveCreativeItem(ItemStack item) {
-    assert mc.interactionManager != null;
-    assert mc.player != null;
-    mc.interactionManager.clickCreativeStack(item, 36 + mc.player.inventory.getSwappableHotbarSlot());
-  }
-  
-  public static void log(Level level, String message) {
-	    LOGGER.log(level, "[" + MOD_NAME + "] " + message);}
-  
-  public static void chat(String text) {
-	    mc.player.sendMessage(new LiteralText(text), false);
-	  }
+   public static void giveCreativeItem(ItemStack item) {
+      assert mc.interactionManager != null;
+      assert mc.player != null;
+      mc.interactionManager
+          .clickCreativeStack(item, 36 + mc.player.inventory.getSwappableHotbarSlot());
+   }
+
+   public static void log(Level level, String message) {
+      LOGGER.log(level, "[" + MOD_NAME + "] " + message);
+   }
+
+   public static void chat(String text) {
+     assert mc.player != null;
+     mc.player.sendMessage(new LiteralText(text), false);
+   }
+
+   @Override
+   public void onInitialize() {
+      log(Level.INFO, "Initializing");
+
+   }
 
 }
