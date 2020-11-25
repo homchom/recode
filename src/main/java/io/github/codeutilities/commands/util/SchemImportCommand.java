fca77a5384b9f1b641a4dc3c3fa6df9a72932f2a
RyanLandDev <46247385+RyanLandDev@users.noreply.github.com>
2020-11-25 20:06:39 +0100

    Update SchemImportCommand.java

--- src/main/java/io/github/codeutilities/commands/util/SchemImportCommand.java
+++ src/main/java/io/github/codeutilities/commands/util/SchemImportCommand.java
@@ -1,1 +1,22 @@
-a
+package io.github.codeutilities.commands.util;
+
+import com.mojang.brigadier.CommandDispatcher;
+import io.github.codeutilities.commands.Command;
+import io.github.codeutilities.commands.arguments.ArgBuilder;
+import io.github.codeutilities.config.ModConfig;
+import io.github.cottonmc.clientcommands.CottonClientCommandSource;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.*;
+
+public class SchemImportCommand extends Command {
+
+    @Override
+    public void register(MinecraftClient mc, CommandDispatcher<CottonClientCommandSource> cd) {
+        cd.register(ArgBuilder.literal("colors").executes((context) -> {
+            mc.player.sendMessage('test', false);
+
+            return 1;
+        }));
+
+    }
+}
