13e3a7997b5d8e112c1dd0d75b758f1d8cfadc88
RyanLandDev <ryantuijp@hotmail.com>
2020-11-26 16:13:25 +0100

    Update SchemImportCommand.java

--- src/main/java/io/github/codeutilities/commands/util/SchemImportCommand.java
+++ src/main/java/io/github/codeutilities/commands/util/SchemImportCommand.java
@@ -13,7 +13,8 @@ public class SchemImportCommand extends Command {
     @Override
     public void register(MinecraftClient mc, CommandDispatcher<CottonClientCommandSource> cd) {
         cd.register(ArgBuilder.literal("schem import").executes((context) -> {
-            mc.player.sendMessage('test', false);
+            assert mc.player != null;
+            mc.player.sendMessage(Text.of("test"), false);
 
             return 1;
         }));
