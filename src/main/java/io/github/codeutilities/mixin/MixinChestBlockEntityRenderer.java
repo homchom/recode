3674800545afb82d24dfbd9b02b4cb6436df73df
BlazeMCworld <62186782+BlazeMCworld@users.noreply.github.com>
2021-02-17 18:07:00 +0100

    Improvements to /templates

---
+++ src/main/java/io/github/codeutilities/mixin/MixinChestBlockEntityRenderer.java
@@ -0,0 +1,42 @@
+package io.github.codeutilities.mixin;
+
+import com.mojang.blaze3d.platform.GlStateManager;
+import io.github.codeutilities.CodeUtilities;
+import io.github.codeutilities.config.ModConfig;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.client.block.ChestAnimationProgress;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import net.minecraft.client.render.block.entity.ChestBlockEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ChestBlockEntityRenderer.class)
+public abstract class MixinChestBlockEntityRenderer<T extends BlockEntity & ChestAnimationProgress> extends
+    BlockEntityRenderer<T> {
+
+    public MixinChestBlockEntityRenderer(
+        BlockEntityRenderDispatcher dispatcher) {
+        super(dispatcher);
+    }
+
+    @Inject(method = "render(Lnet/minecraft/block/entity/BlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;II)V", at = @At("HEAD"), cancellable = true)
+    public void render(T entity, float tickDelta, MatrixStack matrices,
+        VertexConsumerProvider vertexConsumers, int light, int overlay, CallbackInfo ci) {
+        if (ModConfig.getConfig().chestReplacement) {
+            ci.cancel();
+
+            BlockState state = Blocks.BARREL.getDefaultState();
+
+            CodeUtilities.mc.getBlockRenderManager().renderBlockAsEntity(state,matrices,vertexConsumers,light,overlay);
+
+        }
+
+    }
+}
