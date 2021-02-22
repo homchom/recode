05c7d0d38a026ac897dff925135083f255fdfb7b
Reason <Reasonlesss@users.noreply.github.com>
2021-02-22 11:05:50 +0000

    Remove copy from /uuid. You can now click the message to copy the uuid to clipboard.

--- src/main/java/io/github/codeutilities/commands/util/UuidCommand.java
+++ src/main/java/io/github/codeutilities/commands/util/UuidCommand.java
@@ -9,6 +9,11 @@ import io.github.codeutilities.commands.arguments.ArgBuilder;
 import io.github.codeutilities.util.*;
 import io.github.cottonmc.clientcommands.CottonClientCommandSource;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
@@ -16,16 +21,12 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 public class UuidCommand extends Command {
-
+    //todo: make this async
     @Override
     public void register(MinecraftClient mc, CommandDispatcher<CottonClientCommandSource> cd) {
         cd.register(ArgBuilder.literal("uuid")
                 .then(ArgBuilder.argument("username", StringArgumentType.greedyString())
                         .executes(ctx -> {
-                            boolean copy = false;
-                            if (ctx.getArgument("username", String.class).contains(" copy")) {
-                                copy = true;
-                            }
                             String username = ctx.getArgument("username", String.class).replace(" copy", "");
                             String url = "https://mc-heads.net/minecraft/profile/" + username;
                             try {
@@ -37,11 +38,17 @@ public class UuidCommand extends Command {
                                 JsonObject json = new JsonParser().parse(UUIDJson).getAsJsonObject();
                                 String uuid = json.get("id").getAsString();
                                 String fullUUID = StringUtil.fromTrimmed(uuid);
-                                ChatUtil.sendMessage("§eUUID of §b" + username + "§e is §d" + fullUUID + "§e!");
-                                if (copy) {
-                                    ChatUtil.sendMessage("§aThe UUID has been copied to the clipboard!");
-                                    mc.keyboard.setClipboard(fullUUID);
-                                } else if (DFInfo.isOnDF()) {
+                                
+                                Text text = new LiteralText("UUID of ").styled(s -> s.withColor(Formatting.YELLOW))
+                                        .append(new LiteralText(username).styled(s -> s.withColor(Formatting.BLUE)))
+                                        .append(" is ")
+                                        .append(new LiteralText(uuid).styled(s -> s.withColor(Formatting.LIGHT_PURPLE)))
+                                        .append("!").styled(s ->
+                                                s.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new LiteralText("Click to copy to clipboard.")
+                                                        .styled(style -> style.withColor(Formatting.YELLOW))))
+                                                        .withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, fullUUID)));
+                                mc.player.sendMessage(text, false);
+                                if(mc.player.isCreative()) {
                                     mc.player.sendChatMessage("/txt " + fullUUID);
                                 }
                             } catch (IOException e) {
