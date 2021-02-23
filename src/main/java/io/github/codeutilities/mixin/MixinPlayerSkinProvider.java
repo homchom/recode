517d01891d38eedcacbbcc1b522b4a0446a5058f
BlazeMCworld <62186782+BlazeMCworld@users.noreply.github.com>
2021-02-23 10:06:41 +0100

    may have fixed missing texture bug with /heads

--- src/main/java/io/github/codeutilities/mixin/MixinPlayerSkinProvider.java
+++ src/main/java/io/github/codeutilities/mixin/MixinPlayerSkinProvider.java
@@ -3,7 +3,9 @@ package io.github.codeutilities.mixin;
 import com.google.common.hash.Hashing;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture.Type;
+import io.github.codeutilities.CodeUtilities;
 import java.io.File;
+import java.util.Date;
 import net.minecraft.client.texture.AbstractTexture;
 import net.minecraft.client.texture.PlayerSkinProvider;
 import net.minecraft.client.texture.PlayerSkinTexture;
@@ -30,6 +32,8 @@ public class MixinPlayerSkinProvider {
 
     //Complete overwrite for loadSkin method in net.minecraft.client.texture.PlayerSkinProvider
 
+    long loadingTexture = 0;
+
     @Inject(method = "loadSkin(Lcom/mojang/authlib/minecraft/MinecraftProfileTexture;Lcom/mojang/authlib/minecraft/MinecraftProfileTexture$Type;Lnet/minecraft/client/texture/PlayerSkinProvider$SkinTextureAvailableCallback;)Lnet/minecraft/util/Identifier;", at = @At("HEAD"), cancellable = true)
     private void loadSkin(MinecraftProfileTexture profileTexture, Type type,
         @Nullable PlayerSkinProvider.SkinTextureAvailableCallback callback,
@@ -43,20 +47,24 @@ public class MixinPlayerSkinProvider {
                 callback.onSkinTextureAvailable(type, identifier, profileTexture);
             }
         } else {
-            File file = new File(skinCacheDir, string.length() > 2 ? string.substring(0, 2) : "xx");
-            File file2 = new File(file, string);
-            Identifier finalIdentifier = identifier;
-            new Thread(() -> {
-                PlayerSkinTexture playerSkinTexture = new PlayerSkinTexture(file2,
-                    profileTexture.getUrl(), DefaultSkinHelper
-                    .getTexture(), type == Type.SKIN, () -> {
-                    if (callback != null) {
-                        callback.onSkinTextureAvailable(type, finalIdentifier, profileTexture);
-                    }
+            if (loadingTexture < System.currentTimeMillis()-5000) {
+                loadingTexture = System.currentTimeMillis();
+                File file = new File(skinCacheDir, string.length() > 2 ? string.substring(0, 2) : "xx");
+                File file2 = new File(file, string);
+                Identifier finalIdentifier = identifier;
+                new Thread(() -> {
+                    PlayerSkinTexture playerSkinTexture = new PlayerSkinTexture(file2,
+                        profileTexture.getUrl(), DefaultSkinHelper
+                        .getTexture(), type == Type.SKIN, () -> {
+                        if (callback != null) {
+                            callback.onSkinTextureAvailable(type, finalIdentifier, profileTexture);
+                        }
 
-                });
-                this.textureManager.registerTexture(finalIdentifier, playerSkinTexture);
-            }).start();
+                    });
+                    loadingTexture=0;
+                    CodeUtilities.mc.execute(() -> this.textureManager.registerTexture(finalIdentifier, playerSkinTexture));
+                }).start();
+            }
             identifier = DefaultSkinHelper.getTexture();
         }
 
