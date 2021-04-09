d1f98f924a7982f209409af7260e064e446bf2e9
KabanFriends <yuukun2004@protonmail.ch>
2021-04-10 04:23:56 +0900

    player name auto completion for /uuid

--- src/main/java/io/github/codeutilities/commands/util/UuidCommand.java
+++ src/main/java/io/github/codeutilities/commands/util/UuidCommand.java
@@ -3,9 +3,9 @@ package io.github.codeutilities.commands.util;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.StringArgumentType;
 import io.github.codeutilities.commands.Command;
 import io.github.codeutilities.commands.arguments.ArgBuilder;
+import io.github.codeutilities.commands.arguments.types.PlayerArgumentType;
 import io.github.codeutilities.util.ChatType;
 import io.github.codeutilities.util.ChatUtil;
 import io.github.codeutilities.util.DFInfo;
@@ -26,11 +26,10 @@ public class UuidCommand extends Command {
     @Override
     public void register(MinecraftClient mc, CommandDispatcher<CottonClientCommandSource> cd) {
         cd.register(ArgBuilder.literal("uuid")
-            .then(ArgBuilder.argument("username", StringArgumentType.greedyString())
+            .then(ArgBuilder.argument("username", PlayerArgumentType.player())
                 .executes(ctx -> {
                     new Thread(() -> {
-                        String username = ctx.getArgument("username", String.class)
-                            .replace(" copy", "");
+                        String username = ctx.getArgument("username", String.class);
                         String url = "https://mc-heads.net/minecraft/profile/" + username;
                         try {
                             String UUIDJson = IOUtils
@@ -43,12 +42,12 @@ public class UuidCommand extends Command {
                             String uuid = json.get("id").getAsString();
                             String fullUUID = StringUtil.fromTrimmed(uuid);
 
-                            Text text = new LiteralText("§eUUID of §6" + username + " §eis §5" + fullUUID + "§e!")
+                            Text text = new LiteralText("§eUUID of §6" + username + " §eis §b" + fullUUID + "§e!")
                                 .styled(s -> s.withHoverEvent(
                                     new HoverEvent(HoverEvent.Action.SHOW_TEXT,new LiteralText("§eClick to copy to clipboard."))
                                 ).withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, fullUUID)));
                             mc.player.sendMessage(text, false);
-                            if (mc.player.isCreative() && DFInfo.isOnDF()) {
+                            if (mc.player.isCreative() && DFInfo.isOnDF() && DFInfo.currentState == DFInfo.State.DEV) {
                                 mc.player.sendChatMessage("/txt " + fullUUID);
                             }
                         } catch (IOException e) {
