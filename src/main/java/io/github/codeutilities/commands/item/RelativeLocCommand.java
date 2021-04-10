de1a92bf0aae8412c54f6cdeb37a8554254ffa30
KabanFriends <yuukun2004@protonmail.ch>
2021-04-11 03:09:13 +0900

    add creative check in /relativeloc

--- src/main/java/io/github/codeutilities/commands/item/RelativeLocCommand.java
+++ src/main/java/io/github/codeutilities/commands/item/RelativeLocCommand.java
@@ -5,6 +5,8 @@ import com.mojang.brigadier.arguments.FloatArgumentType;
 import io.github.codeutilities.commands.Command;
 import io.github.codeutilities.commands.arguments.ArgBuilder;
 import io.github.codeutilities.commands.arguments.types.StringListArgumentType;
+import io.github.codeutilities.util.ChatType;
+import io.github.codeutilities.util.ChatUtil;
 import io.github.codeutilities.util.ItemUtil;
 import io.github.cottonmc.clientcommands.CottonClientCommandSource;
 import net.minecraft.client.MinecraftClient;
@@ -36,13 +38,18 @@ public class RelativeLocCommand extends Command {
                             .then(ArgBuilder.argument("rot_down", FloatArgumentType.floatArg())
                                 .then(ArgBuilder.argument("rot_right", FloatArgumentType.floatArg())
                                     .executes(ctx -> {
-                                        String target = ctx.getArgument("target", String.class);
-                                        Float forwards = ctx.getArgument("forwards", float.class);
-                                        Float upwards = ctx.getArgument("upwards", float.class);
-                                        Float right = ctx.getArgument("right", float.class);
-                                        Float rot_down = ctx.getArgument("rot_down", float.class);
-                                        Float rot_right = ctx.getArgument("rot_right", float.class);
-                                        return this.run(mc, target, forwards, upwards, right, rot_down, rot_right);
+                                        if (mc.player.isCreative()) {
+                                            String target = ctx.getArgument("target", String.class);
+                                            Float forwards = ctx.getArgument("forwards", float.class);
+                                            Float upwards = ctx.getArgument("upwards", float.class);
+                                            Float right = ctx.getArgument("right", float.class);
+                                            Float rot_down = ctx.getArgument("rot_down", float.class);
+                                            Float rot_right = ctx.getArgument("rot_right", float.class);
+                                            return this.run(mc, target, forwards, upwards, right, rot_down, rot_right);
+                                        }else {
+                                            ChatUtil.sendTranslateMessage("codeutilities.command.require_creative_mode", ChatType.FAIL);
+                                            return -1;
+                                        }
                                     })
                                 )
                             )
