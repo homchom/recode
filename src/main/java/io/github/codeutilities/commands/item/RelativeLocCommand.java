d4dfe330f30deb82260358f25612d67d57e522e4
KabanFriends <yuukun2004@protonmail.ch>
2021-04-10 17:37:20 +0900

    Remove Jeremaster-exclusive features

---
+++ src/main/java/io/github/codeutilities/commands/item/RelativeLocCommand.java
@@ -0,0 +1,14 @@
+package io.github.codeutilities.commands.item;
+
+import com.mojang.brigadier.CommandDispatcher;
+import io.github.codeutilities.commands.Command;
+import io.github.cottonmc.clientcommands.CottonClientCommandSource;
+import net.minecraft.client.MinecraftClient;
+
+public class RelativeLocCommand extends Command {
+
+    @Override
+    public void register(MinecraftClient mc, CommandDispatcher<CottonClientCommandSource> cd) {
+
+    }
+}
