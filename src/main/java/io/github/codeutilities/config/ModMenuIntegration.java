9a3eb26dbc21ff7485aafc282beb41e967808252
RyanLandDev <ryantuijp@hotmail.com>
2021-04-15 12:44:17 +0200

    Fix cloth config 2

--- src/main/java/io/github/codeutilities/config/ModMenuIntegration.java
+++ src/main/java/io/github/codeutilities/config/ModMenuIntegration.java
@@ -2,7 +2,6 @@ package io.github.codeutilities.config;
 
 import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
-import net.minecraft.client.MinecraftClient;
 
 public class ModMenuIntegration implements ModMenuApi {
 
