ac499e2a41a71955ea2b887d28f8b9aa97c8c274
MarcusSlover <mariuszek016@gmail.com>
2021-04-16 11:39:41 +0200

    * Opening menus is no longer asynchronous

--- src/main/java/io/github/codeutilities/util/IMenu.java
+++ src/main/java/io/github/codeutilities/util/IMenu.java
@@ -3,20 +3,14 @@ package io.github.codeutilities.util;
 import io.github.codeutilities.CodeUtilities;
 import io.github.cottonmc.cotton.gui.client.CottonClientScreen;
 import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
-import net.minecraft.client.MinecraftClient;
 
 public interface IMenu {
-    // Don't do this. Opening guis from others threads on a synchronous game is NOT GOOD.
+
     default void openAsync(LightweightGuiDescription gui, String... args) {
         this.open(args);
-        CodeUtilities.EXECUTOR.submit(() -> {
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            MinecraftClient.getInstance().openScreen(new CottonClientScreen(gui));
-        });
+
+        CottonClientScreen cottonClientScreen = new CottonClientScreen(gui);
+        CodeUtilities.MC.openScreen(cottonClientScreen);
     }
 
     void open(String... args);
