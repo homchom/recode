9acdcbe697cae46947bf31126dea5f19fff16c5e
MarcusSlover <mariuszek016@gmail.com>
2021-05-09 22:57:54 +0200

    * Imports aww

--- src/main/java/io/github/codeutilities/gui/menus/CustomHeadMenu.java
+++ src/main/java/io/github/codeutilities/gui/menus/CustomHeadMenu.java
@@ -1,13 +1,13 @@
-package io.github.codeutilities.util.render.gui.menus;
+package io.github.codeutilities.gui.menus;
 
 import com.google.gson.*;
 import io.github.codeutilities.CodeUtilities;
 import io.github.codeutilities.config.CodeUtilsConfig;
+import io.github.codeutilities.gui.IMenu;
+import io.github.codeutilities.gui.widgets.CTextField;
+import io.github.codeutilities.gui.widgets.ItemScrollablePanel;
 import io.github.codeutilities.util.file.ILoader;
 import io.github.codeutilities.util.networking.WebUtil;
-import io.github.codeutilities.util.render.gui.IMenu;
-import io.github.codeutilities.util.render.gui.widgets.CTextField;
-import io.github.codeutilities.util.render.gui.widgets.ItemScrollablePanel;
 import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
 import io.github.cottonmc.cotton.gui.widget.WButton;
 import io.github.cottonmc.cotton.gui.widget.WPlainPanel;
