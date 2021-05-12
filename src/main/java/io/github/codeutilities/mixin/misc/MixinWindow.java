014b1e509b19a8bfdca550eb92d84cc5497e26d8
Reason <Reasonlesss@users.noreply.github.com>
2021-05-12 16:04:32 +0100

    Fixed it should be returning a long not a void.

--- src/main/java/io/github/codeutilities/mixin/misc/MixinWindow.java
+++ src/main/java/io/github/codeutilities/mixin/misc/MixinWindow.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Window.class)
 public class MixinWindow {
 
-    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwCreateWindow(IILjava/lang/CharSequence;JJ)V"))
+    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwCreateWindow(IILjava/lang/CharSequence;JJ)J"))
     public void init(CallbackInfo ci) {
         GLFW.glfwWindowHint(GLFW.GLFW_COCOA_RETINA_FRAMEBUFFER, 0);
     }
