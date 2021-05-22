c58758c34bcd0de15f8bc800dcd777bf38dc1035
Reason <Reasonlesss@users.noreply.github.com>
2021-05-22 14:29:01 +0100

    ColoredText.multiple

--- src/main/java/io/github/codeutilities/util/chat/ColoredText.java
+++ src/main/java/io/github/codeutilities/util/chat/ColoredText.java
@@ -1,8 +1,14 @@
 package io.github.codeutilities.util.chat;
 
+import com.google.common.collect.ImmutableTable;
+import net.minecraft.item.ItemStack;
 import net.minecraft.text.LiteralText;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.TextColor;
 import net.minecraft.util.Formatting;
+
+import java.util.Arrays;
+import java.util.Iterator;
 
 public class ColoredText extends LiteralText {
     public ColoredText(String code, String string) {
@@ -15,6 +21,20 @@ public class ColoredText extends LiteralText {
         }else {
             this.styled(style -> style.withColor(TextColor.fromFormatting(formatting)));
         }
+    }
+
+    public static MutableText multiple(String... str) {
+        MutableText mutableText = new LiteralText("");
+        Iterator<String> stringIterator = Arrays.stream(str).iterator();
+
+        while(stringIterator.hasNext()) {
+            String code = stringIterator.next();
+            String string = stringIterator.next();
+
+            mutableText.append(new ColoredText(code, string));
+        }
+
+        return mutableText;
     }
 
 }
