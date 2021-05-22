b1421bc67b9c34bee9b62b379a296ea167048cbd
Reason <Reasonlesss@users.noreply.github.com>
2021-05-22 14:13:35 +0100

    Formatting Support

--- src/main/java/io/github/codeutilities/util/chat/ColoredText.java
+++ src/main/java/io/github/codeutilities/util/chat/ColoredText.java
@@ -2,14 +2,23 @@ package io.github.codeutilities.util.chat;
 
 import net.minecraft.text.LiteralText;
 import net.minecraft.text.TextColor;
+import net.minecraft.util.Formatting;
 
 public class ColoredText extends LiteralText {
-    public ColoredText(int color, String string) {
+    public ColoredText(TextColor textColor, String string) {
         super(string);
-        this.styled(style -> style.withColor(TextColor.fromRgb(color)));
+        this.styled(style -> style.withColor(textColor));
+    }
+
+    public ColoredText(Formatting formatting, String string) {
+        this(TextColor.fromFormatting(formatting), string);
+    }
+
+    public ColoredText(int color, String string) {
+        this(TextColor.fromRgb(color), string);
     }
 
     public ColoredText(String hex, String string) {
-        this(Integer.parseInt(hex, 16), string);
+        this(TextColor.fromRgb(Integer.parseInt(hex, 16)), string);
     }
 }
