884d6aa67f4479da8a52fe19f67801a1593d8d46
Reason <Reasonlesss@users.noreply.github.com>
2021-05-22 14:20:53 +0100

    ColoredText changes

--- src/main/java/io/github/codeutilities/util/chat/ColoredText.java
+++ src/main/java/io/github/codeutilities/util/chat/ColoredText.java
@@ -5,20 +5,16 @@ import net.minecraft.text.TextColor;
 import net.minecraft.util.Formatting;
 
 public class ColoredText extends LiteralText {
-    public ColoredText(TextColor textColor, String string) {
+    public ColoredText(String code, String string) {
         super(string);
-        this.styled(style -> style.withColor(textColor));
+
+        Formatting formatting = Formatting.byCode(code.charAt(0));
+
+        if(formatting == null) {
+            this.styled(style -> style.withColor(TextColor.fromRgb(Integer.parseInt(code, 16))));
+        }else {
+            this.styled(style -> style.withColor(TextColor.fromFormatting(formatting)));
+        }
     }
 
-    public ColoredText(Formatting formatting, String string) {
-        this(TextColor.fromFormatting(formatting), string);
-    }
-
-    public ColoredText(int color, String string) {
-        this(TextColor.fromRgb(color), string);
-    }
-
-    public ColoredText(String hex, String string) {
-        this(TextColor.fromRgb(Integer.parseInt(hex, 16)), string);
-    }
 }
