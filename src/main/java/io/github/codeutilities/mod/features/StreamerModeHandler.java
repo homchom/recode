ce56e210529b04686d62ba02a82ad9d51ae0c919
RyanLandDev <ryanlandofficial@hotmail.com>
2021-08-04 00:25:32 +0200

    Epic streamer mode update

--- src/main/java/io/github/codeutilities/mod/features/StreamerModeHandler.java
+++ src/main/java/io/github/codeutilities/mod/features/StreamerModeHandler.java
@@ -77,6 +77,8 @@ public class StreamerModeHandler {
     private static final String BUYCRAFT_UPDATE_REGEX = "^A new version of BuycraftX \\([0-9.]+\\) is available\\. Go to your server panel at https://server.tebex.io/plugins to download the update\\.$";
     private static final String PLOT_AD_REGEX = "^.*\\[ Plot Ad ].*\\n.+\\n.*$";
     private static final String SCANNING_REGEX = "^Scanning \\w+(.|\n)*\\[Online] \\[Offline] \\[(IP|)Banned]\1*$";
+    private static final String TELEPORTING_REGEX = "^\\[([^ ]{3,}): Teleported ([^ ]{3,}) to ([^ ]{3,})]$";
+    private static final String JOIN_FAIL_REGEX = "^([^ ]{3,}) tried to join, but is banned \\(.*\\)!$";
 
     private static String getDmRegex(String sender) {
         return "^\\[" + sender + " → You] .+$";
@@ -111,9 +113,17 @@ public class StreamerModeHandler {
                 stripped.startsWith("[Silent]") ||
                 // Incoming reports
                 stripped.startsWith("! Incoming Report ") ||
-                // Scanning
-                stripped.matches(SCANNING_REGEX)
+                // Teleport
+                stripped.matches(TELEPORTING_REGEX) ||
+                // Join fail
+                stripped.matches(JOIN_FAIL_REGEX)
                 )) {
+            return true;
+        }
+
+        // Hide moderation scanning
+        if (hideModeration() && stripped.matches(SCANNING_REGEX)) {
+            MessageGrabber.hideSilently(1);
             return true;
         }
 
