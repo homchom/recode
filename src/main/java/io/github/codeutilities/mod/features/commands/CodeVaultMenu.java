7733ab53af9318ca2b6bfb96e11d50c83724baaa
BlazeMCworld <blazemcworld@users.noreply.github.com>
2021-08-14 20:33:08 +0200

    code vault progress

--- src/main/java/io/github/codeutilities/mod/features/commands/CodeVaultMenu.java
+++ src/main/java/io/github/codeutilities/mod/features/commands/CodeVaultMenu.java
@@ -1,23 +1,34 @@
 package io.github.codeutilities.mod.features.commands;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import io.github.codeutilities.CodeUtilities;
+import io.github.codeutilities.mod.features.social.tab.CodeUtilitiesServer;
+import io.github.codeutilities.mod.features.social.tab.WebMessage;
 import io.github.codeutilities.sys.renderer.IMenu;
 import io.github.codeutilities.sys.renderer.widgets.CTextField;
 import io.github.codeutilities.sys.renderer.widgets.ItemScrollablePanel;
+import io.github.codeutilities.sys.util.ItemUtil;
 import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
 import io.github.cottonmc.cotton.gui.widget.WButton;
 import io.github.cottonmc.cotton.gui.widget.WPlainPanel;
 import io.github.cottonmc.cotton.gui.widget.WScrollPanel;
 import io.github.cottonmc.cotton.gui.widget.WTextField;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Random;
 import java.util.UUID;
 import net.fabricmc.fabric.api.util.TriState;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.text.LiteralText;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class CodeVaultMenu extends LightweightGuiDescription implements IMenu {
@@ -33,17 +44,21 @@ public class CodeVaultMenu extends LightweightGuiDescription implements IMenu {
         "NBS Songs"
     };
 
+    List<ItemStack> items = new ArrayList<>();
+    ItemScrollablePanel panel;
+
     @Override
     public void open(String... args) throws CommandSyntaxException {
+        fetch();
         WPlainPanel root = new WPlainPanel();
         root.setSize(300, 100);
 
         WTextField searchBox = new WTextField(
             new LiteralText("Search..."));
-        searchBox.setMaxLength(100);
+        searchBox.setMaxLength(100);//todo: make changing the query update the items
         root.add(searchBox, 110, 0, 250, 0);
 
-        ItemScrollablePanel panel = ItemScrollablePanel.with(sampleItems());
+        panel = ItemScrollablePanel.with(items);
         panel.setScrollingVertically(TriState.TRUE);
         panel.setScrollingHorizontally(TriState.FALSE);
         root.add(panel, 110, 25, 250, 140);
@@ -52,6 +67,7 @@ public class CodeVaultMenu extends LightweightGuiDescription implements IMenu {
         for (String category : categories) {
             WButton btn = new WButton(new LiteralText(category));
             root.add(btn,0,y,100,22);
+            //todo: make category buttons do something
             y+=21;
         }
 
@@ -59,18 +75,39 @@ public class CodeVaultMenu extends LightweightGuiDescription implements IMenu {
         root.validate(this);
     }
 
-    private List<ItemStack> sampleItems() {
-        List<ItemStack> items = new ArrayList<>();
-        Random rng = new Random();
+    private void fetch() {
+        CodeUtilitiesServer.requestMessage(new WebMessage("code-vault"), message -> {
+            JsonObject content = message.getContent().getAsJsonObject();
+            for (Entry<String, JsonElement> k : content.entrySet()) {
+                try {
+                    JsonArray arr = k.getValue().getAsJsonArray();
+                    String id = k.getKey();//todo: add to item lore
+                    String plotsize = arr.get(0).getAsString();
+                    String category = arr.get(1).getAsString();
+                    String templatedata = arr.get(2).getAsString();//todo: add to item nbt
+                    String lore = arr.get(3).getAsString();//todo: add
+                    String author = arr.get(4).getAsString();
+                    String rank = arr.get(5).getAsString();
+                    String name = arr.get(6).getAsString();
+                    String material = arr.get(7).getAsString();
 
-        int count = rng.nextInt(50)+50;
+                    ItemStack item = new ItemStack(Registry.ITEM.get(new Identifier(material)));
+                    item.setCustomName(new LiteralText(name));
 
-        for (int i = 0; i < count; i++) {
-            ItemStack item = new ItemStack(Registry.ITEM.get(rng.nextInt(100)));
-            item.setCustomName(new LiteralText(UUID.randomUUID().toString()));
-            items.add(item);
-        }
+                    List<String> ilore = new ArrayList<>();
 
-        return items;
+                    ilore.add("§7Created by §a" + author);
+                    ilore.add("§2⚐ Category " + category);
+                    ilore.add("§5☐ §7" + rank + " §5§l! §7" + plotsize);//todo: translate rank & plotsize to string
+
+                    item = ItemUtil.setLore(item, ilore.toArray(new String[0]));
+
+                    items.add(item);
+                } catch (Exception err) {
+                    err.printStackTrace();
+                }
+            }
+            CodeUtilities.MC.submit(() -> panel.setItems(items));
+        });
     }
 }
