c3bf8172f18fff22d9f00d3f60a17ffccb718132
BlazeMCworld <blazemcworld@users.noreply.github.com>
2021-08-27 10:18:56 +0200

    fixed message stacker ignoring colors

--- src/main/java/io/github/codeutilities/sys/util/OrderedTextUtil.java
+++ src/main/java/io/github/codeutilities/sys/util/OrderedTextUtil.java
@@ -1,8 +1,10 @@
 package io.github.codeutilities.sys.util;
 
+import io.github.codeutilities.sys.player.chat.color.MinecraftColors;
 import net.minecraft.text.CharacterVisitor;
 import net.minecraft.text.OrderedText;
 import net.minecraft.text.Style;
+import net.minecraft.text.TextColor;
 
 public class OrderedTextUtil implements CharacterVisitor {
 
@@ -10,6 +12,18 @@ public class OrderedTextUtil implements CharacterVisitor {
 
     @Override
     public boolean accept(int index, Style style, int codePoint) {
+        TextColor color = style.getColor();
+        String code = MinecraftColors.getMcFromFormatting(color);
+        if (code == null) {
+            sb.append(MinecraftColors.hexToMc(String.valueOf(color)));
+        } else {
+            sb.append(code);
+        }
+        if (style.isBold()) sb.append("§l");
+        if (style.isItalic()) sb.append("§o");
+        if (style.isStrikethrough()) sb.append("§n");
+        if (style.isUnderlined()) sb.append("§m");
+        if (style.isBold()) sb.append("§k");
         sb.appendCodePoint(codePoint);
         return true;
     }
