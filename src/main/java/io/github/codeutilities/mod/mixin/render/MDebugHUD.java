2a54c323fc14cc33669833ec957fc6d6deea83f5
TJ20201 <86313252+tj20201@users.noreply.github.com>
2021-12-11 12:57:31 +0000

    Remove imports from testing

--- src/main/java/io/github/codeutilities/mod/mixin/render/MDebugHUD.java
+++ src/main/java/io/github/codeutilities/mod/mixin/render/MDebugHUD.java
@@ -1,16 +1,12 @@
 package io.github.codeutilities.mod.mixin.render;
 
-import io.github.codeutilities.CodeUtilities;
 import io.github.codeutilities.mod.config.Config;
 import io.github.codeutilities.sys.player.DFInfo;
-import jdk.vm.ci.code.CodeUtil;
 import net.minecraft.client.gui.hud.DebugHud;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
