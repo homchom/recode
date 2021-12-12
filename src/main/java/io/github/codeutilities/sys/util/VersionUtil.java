6a8e2d57e92455ae90bf91fd988816476acd5f15
Reasonlesss <28310208+Reasonlesss@users.noreply.github.com>
2021-12-12 13:09:17 +0000

    Only show the prompt if you're 10 builds behind.

--- src/main/java/io/github/codeutilities/sys/util/VersionUtil.java
+++ src/main/java/io/github/codeutilities/sys/util/VersionUtil.java
@@ -3,20 +3,29 @@ package io.github.codeutilities.sys.util;
 import com.google.gson.JsonObject;
 import io.github.codeutilities.CodeUtilities;
 import io.github.codeutilities.sys.networking.WebUtil;
+import net.fabricmc.loader.api.FabricLoader;
 
 import java.io.IOException;
 
 public class VersionUtil {
 
-    public static String getLatestVersion() {
+    public static int getLatestVersion() {
         try {
             String webContent = WebUtil.getString("https://api.github.com/repos/CodeUtilities/CodeUtilities/releases/latest");
             JsonObject jsonObject = CodeUtilities.JSON_PARSER.parse(webContent).getAsJsonObject();
-            return jsonObject.get("name").getAsString().substring(6);
+            return Integer.parseInt(jsonObject.get("name").getAsString().substring(6));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        return 0;
+    }
+
+    public static int getCurrentVersionInt() {
+        try {
+            return Integer.parseInt(CodeUtilities.MOD_VERSION);
+        }catch (NumberFormatException e) {
+            return -1;
+        }
     }
 
 }
