e1ab7cfb56dddace9c76b7206f0173c0a227b125
tk2217 <git@tk2217.com>
2022-07-05 08:44:36 -0500

    ye

--- src/main/java/io/github/homchom/recode/mod/features/social/chat/message/finalizers/DebugFinalizer.java
+++ src/main/java/io/github/homchom/recode/mod/features/social/chat/message/finalizers/DebugFinalizer.java
@@ -3,13 +3,14 @@ package io.github.homchom.recode.mod.features.social.chat.message.finalizers;
 import io.github.homchom.recode.LegacyRecode;
 import io.github.homchom.recode.mod.config.Config;
 import io.github.homchom.recode.mod.features.social.chat.message.*;
+import net.minecraft.network.chat.Component;
 
 public class DebugFinalizer extends MessageFinalizer {
 
     @Override
     protected void receive(Message message) {
         if (Config.getBoolean("debugMode")) {
-            LegacyRecode.info(message.toString());
+            LegacyRecode.info(Component.Serializer.toJson(message.getText()));
         }
     }
 }
