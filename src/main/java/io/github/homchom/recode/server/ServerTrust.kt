86fbe3c53e7baab6341825135f0b1d1286088459
BlazeMCworld <de.blazemcworld@gmail.com>
2022-10-29 14:50:46 +0200

    removes the mod trusting df as that causes to kicks on beta for most chat messages

--- src/main/java/io/github/homchom/recode/server/ServerTrust.kt
+++
@@ -1,6 +0,0 @@
-@file:JvmName("ServerTrust")
-
-package io.github.homchom.recode.server
-
-// TODO: consider custom trusted servers? (perhaps not fitting for the mod though)
-val isServerTrusted get() = isOnDF
