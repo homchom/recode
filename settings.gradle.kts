19d3d3e029e13516d9ce31581ecfd534c49fc004
homchom <morshuchom@gmail.com>
2022-10-31 12:50:26 -0400

    add mavenCentral to plugin repositories and rely less on jcenter

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,11 @@
 pluginManagement {
     repositories {
-        jcenter()
+        mavenCentral()
         maven {
             name = "Fabric"
             url = uri("https://maven.fabricmc.net/")
         }
         gradlePluginPortal()
+        jcenter() // TODO: remove when DiscordRPC dependency is gone
     }
 }
