ff8abf6f2fb7311de55ce28b525ecc375cd644dc
BlazeMCworld <de.blazemcworld@gmail.com>
2022-10-31 17:34:19 +0100

    template peeker base

--- src/main/java/io/github/homchom/recode/feature/RenderingFeatureGroup.kt
+++ src/main/java/io/github/homchom/recode/feature/RenderingFeatureGroup.kt
@@ -2,8 +2,10 @@ package io.github.homchom.recode.feature
 
 import io.github.homchom.recode.feature.rendering.FCodeSearch
 import io.github.homchom.recode.feature.rendering.FSignRenderDistance
+import io.github.homchom.recode.feature.rendering.FTemplatePeeker
 
 val RenderingFeatureGroup = featureGroup("Rendering",
     FSignRenderDistance,
-    FCodeSearch
+    FCodeSearch,
+    FTemplatePeeker
 )
