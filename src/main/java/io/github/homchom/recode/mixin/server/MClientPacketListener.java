104a12f7e5a6684dfd6447f146d0826fce7d1ac4
homchom <morshuchom@gmail.com>
2022-10-31 23:34:21 -0400

    fix chatting on beta

--- src/main/java/io/github/homchom/recode/mixin/server/MClientPacketListener.java
+++ src/main/java/io/github/homchom/recode/mixin/server/MClientPacketListener.java
@@ -1,17 +1,15 @@
 package io.github.homchom.recode.mixin.server;
 
-import io.github.homchom.recode.server.ServerTrust;
+import net.minecraft.client.gui.components.toasts.*;
 import net.minecraft.client.multiplayer.ClientPacketListener;
-import net.minecraft.network.protocol.game.ClientboundServerDataPacket;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.*;
 
 @Mixin(ClientPacketListener.class)
 public class MClientPacketListener {
 	@Redirect(method = "handleServerData", at = @At(value = "INVOKE", target =
-			"Lnet/minecraft/network/protocol/game/ClientboundServerDataPacket;enforcesSecureChat()Z"))
-	public boolean enforcesSecureChatOrIsTrusted(ClientboundServerDataPacket packet) {
-		return packet.enforcesSecureChat() || ServerTrust.isServerTrusted();
+		"Lnet/minecraft/client/gui/components/toasts/ToastComponent;addToast(Lnet/minecraft/client/gui/components/toasts/Toast;)V"))
+	public void hideSecureChatToastIfTrusted(ToastComponent instance, Toast toast) {
+		// do nothing
 	}
 }
