8d3e5abe9606f2bf2d8e0f9d5233ced87da448c4
SamMan56 <111656378+SamMan56@users.noreply.github.com>
2023-05-25 17:39:56 +0100

    add one comment

--- src/main/java/io/github/homchom/recode/server/DFState.kt
+++ src/main/java/io/github/homchom/recode/server/DFState.kt
@@ -11,6 +11,7 @@ val isOnDF get() = mc.currentServer?.ip?.matches(dfIPRegex) ?: false
 
 fun fixDfIp(ip: String): String {
     if (ip.matches(dfIPsRegex)) {
+        // first and second capture groups are any subdomain to ip & port respectively
         return ip.replace(dfIPsRegex, "$1mcdiamondfire.com$2")
     }
     return ip;
