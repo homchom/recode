b471c23119d32902cb79c4c5570b01f966f87598
pomchom <morshuchom@gmail.com>
2023-08-21 16:37:25 -0400

    cleanup and merge #58

--- src/main/java/io/github/homchom/recode/mod/mixin/inventory/MHeldItemTooltip.java
+++ src/main/java/io/github/homchom/recode/mod/mixin/inventory/MHeldItemTooltip.java
@@ -5,12 +5,11 @@ import com.google.gson.JsonParser;
 import io.github.homchom.recode.mod.config.Config;
 import io.github.homchom.recode.mod.features.VarSyntaxHighlighter;
 import io.github.homchom.recode.sys.util.TextUtil;
-import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -18,25 +17,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Objects;
 
 @Mixin(Gui.class)
 public class MHeldItemTooltip {
     @Shadow
     private ItemStack lastToolHighlight;
-    private static final Map<String, MutableComponent> scopes = new HashMap<>();
 
-    static {
-        scopes.put("unsaved",
-                Component.literal("GAME").withStyle((style) -> style.withColor(ChatFormatting.GRAY)));
-        scopes.put("saved",
-                Component.literal("SAVE").withStyle((style) -> style.withColor(ChatFormatting.YELLOW)));
-        scopes.put("local",
-                Component.literal("LOCAL").withStyle((style) -> style.withColor(ChatFormatting.GREEN)));
-    }
-
-    // TODO: improve performance (e.g. don't highlight every iteration)
+    // TODO: remove redundant code and improve performance (e.g. don't highlight every iteration)
     @Inject(method = "renderSelectedItemName", at = @At("HEAD"), cancellable = true)
     public void renderSelectedItemName(GuiGraphics guiGraphics, CallbackInfo callbackInfo) {
         var renderVarScope = Config.getBoolean("variableScopeView");
@@ -62,19 +50,22 @@ public class MHeldItemTooltip {
             var font = Minecraft.getInstance().font;
 
             // render scope
-            var scopeName = varData.get("scope").getAsString();
-            if (type.equals("var") && renderVarScope && scopes.containsKey(scopeName)) {
+            if (type.equals("var") && renderVarScope) {
                 callbackInfo.cancel();
 
                 var name = varData.get("name").getAsString();
-                var scope = scopes.get(scopeName);
-
                 int x1 = (scaledWidth - font.width(Component.literal(name))) / 2;
                 int y1 = scaledHeight - 45;
+                guiGraphics.drawString(font, Component.literal(name), x1, y1, 0xffffff, true);
+
+                var lore = tag.getCompound("display").getList("Lore", Tag.TAG_STRING);
+                if (lore.isEmpty()) return;
+                var scopeJson = tag.getCompound("display")
+                        .getList("Lore", Tag.TAG_STRING)
+                        .getString(0);
+                var scope = Objects.requireNonNull(Component.Serializer.fromJson(scopeJson));
                 int x2 = (scaledWidth - font.width(scope.getVisualOrderText())) / 2;
                 int y2 = scaledHeight - 35;
-
-                guiGraphics.drawString(font, Component.literal(name), x1, y1, 0xffffff, true);
                 guiGraphics.drawString(font, scope, x2, y2, 0xffffff, true);
             }
 
