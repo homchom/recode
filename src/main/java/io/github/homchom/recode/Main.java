393852efaade9281968e00c981546689e5a146a5
pomchom <morshuchom@gmail.com>
2024-01-03 21:31:34 -0500

    gradle 8.3, fabric loader 0.15

--- src/main/java/io/github/homchom/recode/Main.java
+++ src/main/java/io/github/homchom/recode/Main.java
@@ -1,18 +1,11 @@
 package io.github.homchom.recode;
 
-import com.llamalad7.mixinextras.MixinExtrasBootstrap;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
 
 // keep this file as a .java file!
-public final class Main implements ClientModInitializer, PreLaunchEntrypoint {
+public final class Main implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		Recode.INSTANCE.initialize();
-	}
-
-	@Override
-	public void onPreLaunch() {
-		MixinExtrasBootstrap.init();
 	}
 }
